0b999cb69cbe9d8576f90cc291d417cb77eee895
GlenZha <1015285838@qq.com>
2019-01-10 21:38:28 +0800

    add
    build

---
+++ precache-manifest.7ee71af4f3279f9d507103de75c08122.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "feea4fe15187c83bd7437dd5a45dbabd",
+    "url": "/static/media/banner-img.feea4fe1.jpg"
+  },
+  {
+    "revision": "1bf5693df76a7c5ddfad",
+    "url": "/static/js/runtime~main.1bf5693d.js"
+  },
+  {
+    "revision": "eadb2ffc9c44d21d57ea",
+    "url": "/static/js/main.eadb2ffc.chunk.js"
+  },
+  {
+    "revision": "09179401407db435f635",
+    "url": "/static/js/2.09179401.chunk.js"
+  },
+  {
+    "revision": "2a918bbb7e11ed72d58c",
+    "url": "/static/js/1.2a918bbb.chunk.js"
+  },
+  {
+    "revision": "9991a5a97176291a04402ac51c10f5c2",
+    "url": "/index.html"
+  }
+];
